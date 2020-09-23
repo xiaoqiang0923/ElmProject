18e902c0566c017805216f5d5f5e7f9f7da6fc22
xiaoqiang0923 <123948369@qq.com>
2020-09-23 11:05:19 +0800

    首页更新

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,16 @@
-;
 
+
+//导航组件
+let mainnavcpn=Vue.extend({
+    template:'#mainnavtemp'
+});
+let mainnav=Vue.component('mainnav',mainnavcpn);
+
+
+
+
+
+//vue组件
 const vm=new Vue({
     el:"#app",
     data:{
